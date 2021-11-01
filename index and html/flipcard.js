fe0f3401c17bbad45bce1fa13f8e411e798d10e7
robinhoodxxx <karrothutanesh016@gmail.com>
2021-11-01 14:08:07 +0530

    first commit

---
+++ index and html/flipcard.js
@@ -0,0 +1,3 @@
+function cjk(){
+    document.getElementById("demo").innerHTML="FUCK U";
+}
---
+++ index and html/matrix.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+int main(){
+int row ,col,m1[100][100],m2[100][100],i,j;
+cin>>row>>col;
+}
